196897b877c1aaf9f9e64b6f828959edfaf069b8
Phil Wee <cyber_peewee@yahoo.com>
2014-02-03 02:28:17 -0800

    Augment Tweet model with additional properties

--- TwitterTimeline/Tweet.h
+++ TwitterTimeline/Tweet.h
@@ -11,6 +11,14 @@
 @interface Tweet : RestObject
 
 @property (nonatomic, strong, readonly) NSString *text;
+@property (nonatomic, strong, readonly) NSString *name;
+@property (nonatomic, strong, readonly) NSString *screen_name;
+@property (nonatomic, strong, readonly) NSString *profile_image_url;
+@property (nonatomic, readonly) NSUInteger retweet_count;
+@property (nonatomic, readonly) NSUInteger favorite_count;
+@property (nonatomic, readonly) BOOL retweeted;
+@property (nonatomic, readonly) BOOL favorited;
+@property (nonatomic, strong, readonly) NSString *created_at;
 
 + (NSMutableArray *)tweetsWithArray:(NSArray *)array;
 
--- TwitterTimeline/Tweet.m
+++ TwitterTimeline/Tweet.m
@@ -14,6 +14,41 @@
     return [self.data valueOrNilForKeyPath:@"text"];
 }
 
+- (NSString *)name {
+    NSDictionary *user = [self.data valueOrNilForKeyPath:@"user"];
+    return [user valueOrNilForKeyPath:@"name"];
+}
+
+- (NSString *)screen_name {
+    NSDictionary *user = [self.data valueOrNilForKeyPath:@"user"];
+    return [@"@" stringByAppendingString: [user valueOrNilForKeyPath:@"screen_name"] ];
+}
+
+- (NSString *)profile_image_url {
+    NSDictionary *user = [self.data valueOrNilForKeyPath:@"user"];
+    return [user valueOrNilForKeyPath:@"profile_image_url"];
+}
+
+- (NSUInteger)retweet_count {
+    return [[self.data valueOrNilForKeyPath:@"retweet_count"] integerValue];
+}
+
+- (NSUInteger)favorite_count {
+    return [[self.data valueOrNilForKeyPath:@"favorite_count"] integerValue];
+}
+
+- (BOOL)retweeted {
+    return [[self.data valueOrNilForKeyPath:@"retweeted"] boolValue];
+}
+
+- (BOOL)favorited {
+    return [[self.data valueOrNilForKeyPath:@"favorited"] boolValue];
+}
+
+- (NSString *)created_at {
+    return [self.data valueOrNilForKeyPath:@"created_at"];
+}
+
 + (NSMutableArray *)tweetsWithArray:(NSArray *)array {
     NSMutableArray *tweets = [[NSMutableArray alloc] initWithCapacity:array.count];
     for (NSDictionary *params in array) {
